eef1b5efb5e98bc22bcd1dce1ccbec9c4a421077
acabezasg <45835413+acabezasg@users.noreply.github.com>
2019-05-24 08:38:59 -0500

    Add files via upload

--- saleor/settings.py
+++ saleor/settings.py
@@ -380,7 +380,7 @@ bootstrap4 = {
 
 TEST_RUNNER = 'tests.runner.PytestTestRunner'
 
-ALLOWED_HOSTS = ['www.urpicorp.com', 'urpi-final.herokuapp.com', 'localhost', 'urpi-prueba-2.appspot.com']
+ALLOWED_HOSTS = ['www.urpicorp.com', 'urpi-final.herokuapp.com', 'localhost', 'urpi-prueba-2.appspot.com', 'urpicorp.com]
 ALLOWED_GRAPHQL_ORIGINS = os.environ.get('ALLOWED_GRAPHQL_ORIGINS', '*')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
